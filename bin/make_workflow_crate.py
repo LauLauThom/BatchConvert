502f300a9fcc34cf73a52d782da575b5985332fe
Laurent <bricolau22@hotmail.fr>
2025-04-04 12:11:01 +0200

    create BatchConvertWorkflowRunCrate

--- bin/make_workflow_crate.py
+++ bin/make_workflow_crate.py
@@ -19,38 +19,143 @@ from typing import NewType
 BcWorkflowCrate = NewType("BcWorkflowCrate", ROCrate)
 """A pseudo custom type for BatchConvert workflow crate, just to facilitate type checking."""
 
-def createFormalParameter(crate : ROCrate,
-                          id: str, 
-                          additionalType : Literal["Text", "Boolean", "Integer", "Dataset", "File"], 
-                          name:str, 
-                          description : Optional[str] = None,
-                          valueRequired = False,
-                          defaultValue = None) -> ContextEntity:
-    """
-    Create a FormalParameter to describe an input or output of a workflow.  
-    The FormalParameter is created in the crate as a separate entity, and returned by the function.  
-    The caller should then associate the returned FormalParameter entity to the workflow of interest. 
-    """
-    properties = {"@type": "FormalParameter",
-                  "additionalType" : additionalType,
-                  "valueRequired" : valueRequired,
-                  "conformsTo": {
-                            "@id": "https://bioschemas.org/profiles/FormalParameter/1.0-RELEASE"
-                            },
-                  "name" : name
-                 }
+class BatchConvertWorkflowRunCrate(ROCrate):
+    """Custom class for the BatchConvertWorkflowRunCrate, adding convenience functions."""
     
-    if description:
-        properties["description"] = description
+    def __init__(self, repo_root_dir : str):
+        """Create a workflow crate for BatchConvert, call add_input... to add additional infos about a run."""
+        super().__init__()
 
-    if defaultValue:
-        properties["defaultValue"] = defaultValue
+        # crate.add_workflow("batchconvert", main=True, lang = "shell") # throws an error, currently lang has to be one of "cwl", "galaxy", "knime", "nextflow", "snakemake", "compss", "autosubmit"
+        # However one can also pass a ComputerLanguage object to lang, see https://github.com/ResearchObject/ro-crate-py/issues/218#issuecomment-2753694857
+        bash_url = "https://www.gnu.org/software/bash/"
+        bash = ComputerLanguage(self, identifier="#bash", properties = {"name" : "Bash",
+                                                                        "identifier": {"@id": bash_url},
+                                                                        "url": {"@id": bash_url}
+                                                                        })
+        self.add(bash) # need to add the item first then reference it
 
-    return cast(ContextEntity, crate.add(ContextEntity(crate, 
-                                                        identifier = id if id.startswith("#") else f"#{id}",
-                                                        properties = properties)))
+        # the wf file will be copied from the source, to the directory where the crate will be saved (using crate.write)
+        main_wf = cast(Entity, self.add_workflow(source = os.path.join(repo_root_dir, "batchconvert"), 
+                                                main=True, 
+                                                lang = bash))  # type: ignore
 
-def create_workflow_crate(repo_root_dir:str) -> BcWorkflowCrate:
+        # Create input parameters
+        param_format = self.addFormalParameter(id = "#conversion_format",
+                                               additionalType = "Text",
+                                               name = "conversion_format",
+                                               valueRequired = True,
+                                               description = "Either 'ometiff' or 'omezarr'")
+        
+        param_src_dir = self.addFormalParameter(id = "#src_dir",
+                                                additionalType = "Text",
+                                                name = "src_dir",
+                                                valueRequired = True,
+                                                description = "Input directory with images to convert.") 
+        
+        param_dst_dir = self.addFormalParameter(id = "#dest_dir",
+                                                additionalType = "Text",
+                                                name = "dest_dir",
+                                                valueRequired = True,
+                                                description = "Output directory with converted images.") 
+        
+        
+        param_merge_files = self.addFormalParameter(id = "#merge_files",
+                                                    additionalType = "Boolean",
+                                                    name = "merge_files",
+                                                    valueRequired = False,
+                                                    defaultValue = False,
+                                                    description = "Flag --merge_files, if multiple source files should be concatenated into a single ome-tiff/ome-zarr. Can be used together with the concatenation_order parameter to instruct the dimensions, otherwise batchconvert will automatically group datasets (see the doc).")
+            
+        param_concatenation_order = self.addFormalParameter(id = "#concatenation_order",
+                                                            additionalType = "Text",
+                                                            name = "concatenation_order",
+                                                            valueRequired = False,
+                                                            defaultValue = "auto",
+                                                            description = "Can be used with the --merge_files flag to pass custom dimensions specifiers.")
+                        
+        main_wf["input"] = [param_format, 
+                            param_src_dir,
+                            param_dst_dir,
+                            param_merge_files, 
+                            param_concatenation_order]
+
+        # Adding the subworkflows has part of the WF crate
+        # For a run, one could omit the one not used, or only mention the one used via a createAction
+        # Same process, create the entities and add them to the crate (done in one go by add_workflow here), then link them to the main wf
+        workflow_tiff = self.add_workflow(source = os.path.join(repo_root_dir, "pff2omezarr.nf"),
+                                          main = False,
+                                          lang = "nextflow")
+
+        workflow_zarr = self.add_workflow(source = os.path.join(repo_root_dir, "pff2ometiff.nf"), 
+                                          main = False,
+                                          lang = "nextflow")
+
+        # Add description to the secondary workflow
+        desc_template = "Nextflow workflow executed when passing the argument (i.e converting to) '{}' as first argument to the BatchConvert utility."
+        workflow_tiff["description"] = desc_template.format("ometiff") # type: ignore
+        workflow_zarr["description"] = desc_template.format("omezarr") # type: ignore
+
+        main_wf["hasPart"] = [workflow_tiff, workflow_zarr]  # type: ignore
+        main_wf["url"]     = ["https://github.com/Euro-BioImaging/BatchConvert"]
+
+        # Add the authors and institution potentially
+        # works but not added as author of the workflow
+        # TODO add more authors
+        author = self.add(Person(self, 
+                                identifier="https://orcid.org/0000-0001-9823-0581", 
+                                properties={"name" : "Bugra Özdemir"})) # the O with umlaut gets a unicode character code, which is OK in a UI it renders properly
+        #crate["Authors"] = author # nope
+
+        # Add the authors at the root of the dataset entity as expected by workflow hub
+        """
+        for e in crate.get_entities():
+            
+            if e.id == "./" :
+                root_dataset_entity = e
+                break
+
+        else: # for/else : hit if the for loop does not break
+            raise Exception("Could not find root Dataset entity")
+        """
+        self.root_dataset["author"] = [author]
+
+        # Save the crate in a new subdirectory, all files and directories listed in the json will get copied to the subdirectory
+        #crate.write(rocrate_name) # write and write_crate are the same
+        #crate.write_crate("my_crate")
+
+    def addFormalParameter(self,
+                           id: str, 
+                           additionalType : Literal["Text", "Boolean", "Integer", "Dataset", "File"], 
+                           name:str, 
+                           description : Optional[str] = None,
+                           valueRequired = False,
+                           defaultValue = None) -> ContextEntity:
+        """
+        Create a FormalParameter to describe an input or output of a workflow.  
+        The FormalParameter is created in the crate as a separate entity, and returned by the function.  
+        The caller should then associate the returned FormalParameter entity to the workflow of interest. 
+        """
+        properties = {"@type": "FormalParameter",
+                    "additionalType" : additionalType,
+                    "valueRequired" : valueRequired,
+                    "conformsTo": {
+                                "@id": "https://bioschemas.org/profiles/FormalParameter/1.0-RELEASE"
+                                },
+                    "name" : name
+                    }
+        
+        if description:
+            properties["description"] = description
+
+        if defaultValue:
+            properties["defaultValue"] = defaultValue
+
+        return cast(ContextEntity, self.add(ContextEntity(self, 
+                                                          identifier = id if id.startswith("#") else f"#{id}",
+                                                          properties = properties)))
+
+def create_workflow_crate(repo_root_dir:str) -> BatchConvertWorkflowRunCrate:
     """
     Create a Workflow RO crate representing the BatchConvert implementation.  
     It describes the entry point executable (batchconvert), the nextflow conversion workflow(s) and some parameters that can be passed to the batchconvert command.  
@@ -67,114 +172,7 @@ def create_workflow_crate(repo_root_dir:str) -> BcWorkflowCrate:
     Directory for the root entity of the crate.  
     Elements added to the crate will have their identifier (path) relative to this root.  
     """
-    # Create a crate
-    # Files and folder will be added manually
-    crate = cast(BcWorkflowCrate, ROCrate()) # dont pass a source here, it is either used with init = True to list all files in an existing dir (and subdir), or used to load an existing crate
-
-    # crate.add_workflow("batchconvert", main=True, lang = "shell") # throws an error, currently lang has to be one of "cwl", "galaxy", "knime", "nextflow", "snakemake", "compss", "autosubmit"
-    # However one can also pass a ComputerLanguage object to lang, see https://github.com/ResearchObject/ro-crate-py/issues/218#issuecomment-2753694857
-    bash_url = "https://www.gnu.org/software/bash/"
-    bash = ComputerLanguage(crate, identifier="#bash", properties = {"name" : "Bash",
-                                                                    "identifier": {"@id": bash_url},
-                                                                    "url": {"@id": bash_url}
-                                                                    })
-    crate.add(bash) # need to add the item first then reference it
-
-    # the wf file will be copied from the source, to the directory where the crate will be saved (using crate.write)
-    main_wf = cast(Entity, crate.add_workflow(source = os.path.join(repo_root_dir, "batchconvert"), 
-                                              main=True, 
-                                              lang = bash))  # type: ignore
-
-    # Create input parameters
-    param_format = createFormalParameter(crate,
-                                         id = "#conversion_format",
-                                         additionalType = "Text",
-                                         name = "conversion_format",
-                                         valueRequired = True,
-                                         description = "Either 'ometiff' or 'omezarr'")
-    
-    param_src_dir = createFormalParameter(crate,
-                                          id = "#src_dir",
-                                          additionalType = "Text",
-                                          name = "src_dir",
-                                          valueRequired = True,
-                                          description = "Input directory with images to convert.") 
-    
-    param_dst_dir = createFormalParameter(crate,
-                                         id = "#dest_dir",
-                                         additionalType = "Text",
-                                         name = "dest_dir",
-                                         valueRequired = True,
-                                         description = "Output directory with converted images.") 
-    
-    
-    param_merge_files = createFormalParameter(crate,
-                                              id = "#merge_files",
-                                              additionalType = "Boolean",
-                                              name = "merge_files",
-                                              valueRequired = False,
-                                              defaultValue = False,
-                                              description = "Flag --merge_files, if multiple source files should be concatenated into a single ome-tiff/ome-zarr. Can be used together with the concatenation_order parameter to instruct the dimensions, otherwise batchconvert will automatically group datasets (see the doc).")
-    
-    param_concatenation_order = createFormalParameter(crate,
-                                                      id = "#concatenation_order",
-                                                      additionalType = "Text",
-                                                      name = "concatenation_order",
-                                                      valueRequired = False,
-                                                      defaultValue = "auto",
-                                                      description = "Can be used with the --merge_files flag to pass custom dimensions specifiers.")
-                
-    main_wf["input"] = [param_format, 
-                        param_src_dir,
-                        param_dst_dir,
-                        param_merge_files, 
-                        param_concatenation_order]
-
-    # Adding the subworkflows has part of the WF crate
-    # For a run, one could omit the one not used, or only mention the one used via a createAction
-    # Same process, create the entities and add them to the crate (done in one go by add_workflow here), then link them to the main wf
-    workflow_tiff = crate.add_workflow(source = os.path.join(repo_root_dir, "pff2omezarr.nf"),
-                                       main = False,
-                                       lang = "nextflow")
-
-    workflow_zarr = crate.add_workflow(source = os.path.join(repo_root_dir, "pff2ometiff.nf"), 
-                                       main = False,
-                                       lang = "nextflow")
-
-    # Add description to the secondary workflow
-    desc_template = "Nextflow workflow executed when passing the argument (i.e converting to) '{}' as first argument to the BatchConvert utility."
-    workflow_tiff["description"] = desc_template.format("ometiff") # type: ignore
-    workflow_zarr["description"] = desc_template.format("omezarr") # type: ignore
-
-    main_wf["hasPart"] = [workflow_tiff, workflow_zarr]  # type: ignore
-    main_wf["url"]     = ["https://github.com/Euro-BioImaging/BatchConvert"]
-
-    # Add the authors and institution potentially
-    # works but not added as author of the workflow
-    # TODO add more authors
-    author = crate.add(Person(crate, 
-                              identifier="https://orcid.org/0000-0001-9823-0581", 
-                              properties={"name" : "Bugra Özdemir"})) # the O with umlaut gets a unicode character code, which is OK in a UI it renders properly
-    #crate["Authors"] = author # nope
-
-    # Add the authors at the root of the dataset entity as expected by workflow hub
-    """
-    for e in crate.get_entities():
-        
-        if e.id == "./" :
-            root_dataset_entity = e
-            break
-
-    else: # for/else : hit if the for loop does not break
-        raise Exception("Could not find root Dataset entity")
-    """
-    crate.root_dataset["author"] = [author]
-
-    # Save the crate in a new subdirectory, all files and directories listed in the json will get copied to the subdirectory
-    #crate.write(rocrate_name) # write and write_crate are the same
-    #crate.write_crate("my_crate")
-
-    return crate
+    return BatchConvertWorkflowRunCrate(repo_root_dir)
 
 def add_output_dataset(crate: ROCrate, output_image_dir : str|Path):
     """
